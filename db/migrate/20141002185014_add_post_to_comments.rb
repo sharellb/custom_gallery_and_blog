80538d3bcbf65b7a145b10a5d541ac86d14628bf
Sharell Bryant <sharell.bryant@gmail.com>
2014-11-03 16:53:49 -0500

    Edit migrations

--- db/migrate/20141002185014_add_post_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddPostToComments < ActiveRecord::Migration
-  def change
-  	add_column :comments, :post_id, :integer
-  end
-end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	post = Post.new
+	assert_not post.save
 end
